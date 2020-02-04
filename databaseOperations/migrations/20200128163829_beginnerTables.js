33a4dc0c240974a58ca993eaa2645e39d50c4771
theolamide <ajadidan@gmail.com>
2020-02-03 18:29:38 -0700

    Tables reorederd and migrations run

--- databaseOperations/migrations/20200128163829_beginnerTables.js
+++ databaseOperations/migrations/20200128163829_beginnerTables.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
         users.string('username', 255).notNullable().unique();
         users.string('password', 255).notNullable();
         users.boolean('seller').defaultTo(false).notNullable();
+        users.string('stripe_account', 255).notNullable();
         users.string('address1', 255).notNullable();
         users.string('address2', 255).notNullable();
         users.string('city', 255).notNullable();
@@ -37,80 +38,58 @@ exports.up = function(knex) {
         stores.date('date_created').defaultTo(Date.now()).notNullable();
         stores.date('date_updated').defaultTo(Date.now()).notNullable();
     })
-
-    .createTable("category", category => {
-        category.increments('categoryID').primary();
-        category.string('category_name', 255).unique().notNullable();
-        category.string('decription', 255).notNullable();
-        category.string('picture', 255);
-        category.boolean('active').defaultTo(false);
+    
+    .createTable("quotes", quotes => {
+        quotes.increments('orderID').primary();
+        quotes.decimal('total', null).notNullable();
+        quotes.decimal('subtotal', null).notNullable();
+        quotes.decimal('tax', null).notNullable();
+        quotes.decimal('fees', null).notNullable();
+        quotes.decimal('shipping', null).notNullable();
+        quotes.string('orderToken', 255).unique().notNullable();
+        quotes.string('warnings', 255).notNullable();
+        quotes.string('mode', 255).notNullable();
+        quotes.string('store_name', 255).notNullable().references('store_name').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
+        quotes.string('username', 255).notNullable().references('username').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     })
 
-    .createTable("products", products => {
-        products.increments('productID').primary();        
-        products.boolean('active').defaultTo(true);
-        products.integer('SKU');
-        products.string('product_name', 50).notNullable();
-        products.string('product_description', 500).notNullable();
-        products.integer('quantity_per_unit').defaultTo(1);
-        products.decimal('unit_price', null).notNullable();
-        products.decimal('markup_price', null).notNullable();
-        products.string('available_size', 20);
-        products.string('size', 20).notNullable();
-        products.string('available_color', 20);
-        products.string('color', 20);
-        products.decimal('discount', null).notNullable();
-        products.boolean('product_available').defaultTo(true);
-        products.boolean('discount_available').defaultTo(false);
-        products.string('picture', 255).notNullable();
-        products.string('note', 500);
-        products.integer('store_name').unsigned().notNullable().references('store_name').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
-        products.integer('category_name').unsigned().notNullable().references('category_name').inTable('category').onUpdate('CASCADE').onDelete('CASCADE');
+    
+    .createTable("users_store", users_store => {
+        users_store.increments('user_storeID').primary();
+        users_store.boolean('admin').defaultTo(false);
+        users_store.string('store_name', 255).notNullable().references('store_name').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
+        users_store.string('username', 255).notNullable().references('username').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     })
-
+    
     .createTable("orders", orders => {
         orders.increments('orderID').primary();
-        orders.string('shipper', 255).notNullable();
-        orders.integer('order_number').notNullable().unique();
-        orders.integer('payment_number').notNullable();
-        orders.date('order_date').notNullable();
-        orders.date('payment_date').notNullable();
-        orders.date('ship_date').notNullable();
-        orders.timestamp('time_stamp').defaultTo(knex.fn.now());
-        orders.string('err_message', 255);
-        orders.boolean('paid').defaultTo(false);
-        orders.integer('userID').unsigned().notNullable().references('userID').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
+        orders.string('status', 255).notNullable();
+        orders.decimal('total', null).notNullable();
+        orders.decimal('subtotal', null).notNullable();
+        orders.decimal('tax', null).notNullable();
+        orders.decimal('fees', null).notNullable();
+        orders.decimal('shipping', null).notNullable();
+        orders.string('orderToken', 255).unique().notNullable();       
+        orders.string('mode', 255).notNullable();
+        orders.string('createdAt', 255).notNullable();
+        orders.string('store_name', 255).notNullable().references('store_name').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
+        orders.string('username', 255).notNullable().references('username').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     })
 
-    .createTable("users_store", users_store => {
-        users_store.increments('users_storeID').primary();
-        users_store.boolean('admin').defaultTo(false);
-        users_store.integer('store_name').unsigned().notNullable().references('store_name').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
-        users_store.integer('username').unsigned().notNullable().references('username').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
+    .createTable("design_urls", design_urls => {
+        design_urls.increments('designID').primary();
+        design_urls.string('design_url', 255).notNullable();
+        design_urls.string('store_name', 255).notNullable().references('store_name').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
+        design_urls.string('username', 255).notNullable().references('username').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     })
-
-    .createTable("order_details", order_details => {
-        order_details.increments('order_detailID').primary();
-        order_details.integer('price').notNullable();
-        order_details.integer('quantity').notNullable();
-        order_details.decimal('discount', null);
-        order_details.integer('total').notNullable();
-        order_details.string('size', 50).notNullable();
-        order_details.string('color', 50).notNullable();
-        order_details.string('ship_date', 50).notNullable();
-        order_details.integer('storeID').unsigned().notNullable().references('storeID').inTable('stores').onUpdate('CASCADE').onDelete('CASCADE');
-        order_details.integer('productID').unsigned().notNullable().references('productID').inTable('products').onUpdate('CASCADE').onDelete('CASCADE');
-        order_details.integer('orderID').unsigned().notNullable().references('orderID').inTable('orders').onUpdate('CASCADE').onDelete('CASCADE');
-    });
 };
 
 exports.down = function(knex) {
     return knex.schema
-    .dropTableIfExists("order_details")
-    .dropTableIfExists("users_store")
+    .dropTableIfExists("design_urls")
     .dropTableIfExists("orders")
-    .dropTableIfExists("products")
-    .dropTableIfExists("category")
+    .dropTableIfExists("users_store")
+    .dropTableIfExists("quotes")
     .dropTableIfExists("stores")
     .dropTableIfExists("users");
 };
