11cbdd5a6f5da19e587f42a51894a9eea1b4e14a
theolamide <ajadidan@gmail.com>
2020-02-07 12:02:42 -0700

    Database refactored for codeclimate

--- databaseOperations/seeds/01-users.js
+++ databaseOperations/seeds/01-users.js
@@ -1,4 +1,4 @@
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knex("users").insert([
     {
       first_name: "Merch",
@@ -14,16 +14,7 @@ exports.seed = function(knex) {
       zip_code: 80204,
       country: "USA",
       phone: 2165274556,
-      email: "merchdropper20@gmail.com",
-      billing_address: "7822 Myriad Drive",
-      billing_city: "Denver",
-      billing_zip_code: 80204,
-      billing_country: "USA",
-      shipping_address: "7822 Myriad Drive",
-      shipping_city: "Denver",
-      shipping_zip_code: 80204,
-      shipping_country: "USA",
-      support_pin: "HRYI7820"
+      email: "merchdropper20@gmail.com", billing_address: "7822 Myriad Drive", billing_city: "Denver", billing_zip_code: 80204, billing_country: "USA", shipping_address: "7822 Myriad Drive", shipping_city: "Denver", shipping_zip_code: 80204, shipping_country: "USA", support_pin: "HRYI7820"
     }
   ]);
 };
