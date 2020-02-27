68a771a495db8c5c5739cd3debf26d693d3f77b9
unknown <the.goose.moose@gmail.com>
2020-02-27 04:30:41 -0700

    tweaked design table to allow for thumbnails

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,35 @@
 module.exports = {
-  // development: {
-  //   client: 'pg',
-  //   connection: 'postgres://localhost:5432/',
-  //   pool: {
-  //     min: 2,
-  //     max: 10},
-  //   migrations: {
-  //     directory: './databaseOperations/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './databaseOperations/seeds'
-  //   },
-  //   useNullAsDefault: true
-  // },
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./databaseOperations/database.db3"
-    },
+    client: 'pg',
+    connection: 'postgres://localhost:5432/',
     pool: {
       min: 2,
-      max: 10
-    },
+      max: 10},
     migrations: {
-      directory: "./databaseOperations/migrations"
+      directory: './databaseOperations/migrations'
     },
     seeds: {
-      directory: "./databaseOperations/seeds"
-    }
+      directory: './databaseOperations/seeds'
+    },
+    useNullAsDefault: true
   },
+  // development: {
+  //   client: "sqlite3",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: "./databaseOperations/database.db3"
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: "./databaseOperations/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./databaseOperations/seeds"
+  //   }
+  // },
   ////////////////////////////////////////////////////////
 
   testing: {
