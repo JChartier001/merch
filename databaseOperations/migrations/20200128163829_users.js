43ceef74f60e326b313eb141437e7a942080b2c1
mpaolodr <mpaolodr_1504@yahoo.com>
2020-03-23 16:16:15 -0400

    made some fields nullable in users table

--- databaseOperations/migrations/20200128163829_users.js
+++ databaseOperations/migrations/20200128163829_users.js
@@ -1,24 +1,21 @@
 exports.up = function(knex) {
   return knex.schema.createTable("users", users => {
     users.increments("id").primary();
-    users.string("first_name", 255).notNullable();
-    users.string("last_name", 255).notNullable();
-    users
-      .string("username", 255)
-      .notNullable()
-      .unique();
+    users.string("first_name", 255);
+    users.string("last_name", 255);
+    users.string("username", 255).unique();
     users.string("password", 255).notNullable();
     users
       .boolean("seller")
       .defaultTo(false)
       .notNullable();
-    users.string("stripe_account", 255).notNullable();
-    users.string("address1", 255).notNullable();
+    users.string("stripe_account", 255);
+    users.string("address1", 255);
     users.string("address2", 255).defaultTo("-");
-    users.string("city", 255).notNullable();
-    users.string("state", 255).notNullable();
-    users.integer("zip_code").notNullable();
-    users.string("country", 255).notNullable();
+    users.string("city", 255);
+    users.string("state", 255);
+    users.integer("zip_code");
+    users.string("country", 255);
     users.bigint("phone");
     users.string("email", 255).notNullable();
     users.string("billing_address", 255);
