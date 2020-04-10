43f74a30ac710e5bbad05cdd0131636f4d9e4134
Jonathan Hanson <jhanson8@uoregon.edu>
2020-04-10 14:10:12 -0700

    edit seeds

--- databaseOperations/seeds/02-stores.js
+++ databaseOperations/seeds/02-stores.js
@@ -5,6 +5,12 @@ exports.seed = function(knex) {
       store_name: "Anthill Store",
       userID: 1,
       domain_name: "anthillstore"
+    },
+    {
+      active: true,
+      store_name: "The Best Store",
+      userID: 2,
+      domain_name: "TheBestStore"
     }
   ]);
 };
--- databaseOperations/seeds/06-products.js
+++ databaseOperations/seeds/06-products.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   return knex("products").insert([
     {
-      productName: "Test Product",
+      productName: "Test Product - Store One",
       fullSizeURL:
         "https://res.cloudinary.com/dze74ofbf/image/upload/v1581453585/wsjikfpellbybgzbymy2.jpg",
       thumbnailURL:
@@ -10,6 +10,17 @@ exports.seed = function(knex) {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare arcu vulputate arcu suscipit venenatis. Donec sit amet ipsum ac urna dignissim euismod a euismod nisi. Nullam pulvinar odio semper.",
       price: 10.3,
       storeID: 1
+    },
+    {
+      productName: "Test Product - Store 2",
+      fullSizeURL:
+        "https://res.cloudinary.com/dze74ofbf/image/upload/v1581453585/wsjikfpellbybgzbymy2.jpg",
+      thumbnailURL:
+        "https://res.cloudinary.com/dze74ofbf/image/upload/v1581454280/eiz7lg8c8mtosndddelk.jpg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ornare arcu vulputate arcu suscipit venenatis. Donec sit amet ipsum ac urna dignissim euismod a euismod nisi. Nullam pulvinar odio semper.",
+      price: 22.3,
+      storeID: 2
     }
   ]);
 };
