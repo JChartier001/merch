e681a627286e46bed53b83f2712a27675ebe0862
Landon Turner <lturner@teamworks.com>
2020-05-20 13:39:51 -0400

    adds payment intent object. commented out charge object

--- crudOperations/paymentOperations/paymentRouter.js
+++ crudOperations/paymentOperations/paymentRouter.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config({ path: "./c
 const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST_KEY); //Change STRIPE_SECRET_TEST_KEY to STRIPE_SECRET_KEY to collect payments when stripe goes LIVE.
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     // console.log('body sent to endpoint', req.body)
     const Data = {
         source: req.body.token.id,
@@ -24,15 +24,22 @@ router.post('/', (req, res) => {
             name: req.body.card.name
         }
     };
-
-    stripe.charges.create(Data, (stripeErr, stripeRes) => {
-        if (stripeErr) {
-            res.status(500).json({ error: stripeErr });
-            console.log('Stripe Error',stripeErr)
-        } else {
-            res.status(200).json({ success: stripeRes })
-        }
+    const paymentIntent = await stripe.paymentIntents.create({
+     amount: Data.amount,
+     currency: Data.currency,
+     payment_method_types: ['card'],
+     application_fee_amount: 1 * 100  // placeholder value 
+    }, {
+        stripeAccount: `{{${CONNECTED_STRIPE_ACCOUNT_ID_TEST}}}`
     })
+    // stripe.charges.create(Data, (stripeErr, stripeRes) => {
+    //     if (stripeErr) {
+    //         res.status(500).json({ error: stripeErr });
+    //         console.log('Stripe Error',stripeErr)
+    //     } else {
+    //         res.status(200).json({ success: stripeRes })
+    //     }
+    // })
 
 })
 
