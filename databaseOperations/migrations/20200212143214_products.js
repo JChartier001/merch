5111fdb490086ec18f9d6764612cbd8348ebf4b0
JChartier001 <JChartier001@gmail.com>
2020-05-22 16:38:28 -0500

    rollback migration - added designId to products table

--- databaseOperations/migrations/20200212143214_products.js
+++ databaseOperations/migrations/20200212143214_products.js
@@ -6,6 +6,7 @@ exports.up = function(knex) {
     products.string("thumbnailURL", 500).notNullable();
     products.string("description", 500).defaultTo("None");
     products.decimal("price").defaultTo(0);
+    products.string('desginId').notNullable();
     products
       .integer("storeID")
       .notNullable()
